a11fc21d9b8c06917869aa589a323d112f312f76
Matheus Pecinalli <pecinallipoker@gmail.com>
2023-06-26 10:26:22 -0300

    Catalagos de carro adicionado

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 const CustomButton = ({
   title,
   containerStyles,
-  handleClick, btnType
+  handleClick, btnType, textStyles, rightIcon 
 }: CustomButtonProps) => {
   return (
     <button
@@ -15,7 +15,17 @@ const CustomButton = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className='relative w-6 h-6'>
+          <Image 
+          src={rightIcon}
+          alt='Icone pra direita'
+          fill
+          className='object-contain'
+          />
+        </div>
+      )}
     </button>
   );
 };
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -22,7 +22,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
 
   return (
     <div className='search-manufacturer'>
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relative w-full'>
           <Combobox.Button className='absolute top-[14px]'>
             <Image 
@@ -50,7 +50,37 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
           afterLeave={() => setQuery('')}
           >
             <Combobox.Options>
-
+              {filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                  key={item}
+                  className={({active}) =>`
+                  relative search-manufacturer__option
+                  ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}
+                  `}
+                  value={item}
+                  >
+                    {({selected, active}) =>(
+                      <>
+                        <span
+                            className={`block truncate ${
+                              selected ? 'font-medium' : 'font-normal'
+                            }`}
+                          >
+                            {item}
+                          </span>
+                          {selected ? (
+                            <span
+                              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                active ? 'text-white' : 'text-teal-600'
+                              }`}
+                            >
+                            </span>
+                          ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                )
+              )}
             </Combobox.Options>
           </Transition>
         </div>
